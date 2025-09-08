67f7baf238e73430858ba2d39440686f1c301580
Hemachandra P <hemubtech@gmail.com>
2025-09-08 22:23:26 +0530

    Add files via upload

---
+++ A1_LANDS_PHP/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/*--------------------*/
+// Description: Evora - Real Estate CMS
+// Author: Evora
+// Author URI: https://wicombit.com
+/*--------------------*/
+
+//URL PROJECT
+
+define ('SITE_URL', 'https://a1lands.com');
+
+//DATABASE CONFIG
+
+$database = array(
+'host' => 'localhost',
+'db' => 'u626111305_a1lands',
+'user' => 'u626111305_a1lands',
+'pass' => 'A1lands@123'
+);
+
+?>
